383edc7b36e7452d043949a3b235e412127a295b
ksk-M <bestwedd1@gmail.com>
2024-09-09 23:08:05 +0900

    [add] i18n日本語化

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,7 @@ module Prefectour
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.i18n.default_locale = :ja
     config.generators do |g|
       g.test_framework :rspec,
         view_specs: false
