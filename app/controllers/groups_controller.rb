e1952534d0e45440c0546ed9984898ef6c7576de
ksk-M <bestwedd1@gmail.com>
2024-09-15 12:26:57 +0900

    [add] GroupモデルにCRUD処理を実装

---
+++ app/controllers/groups_controller.rb
@@ -0,0 +1,48 @@
+class GroupsController < ApplicationController
+  def new
+    @group = Group.new
+  end
+
+  def create
+    @group = Group.new(group_params)
+    if @group.save
+      flash[:notice] = "グループを登録しました"
+      redirect_to user_path(current_user.id)
+    else
+      flash.now[:alert] = "グループの登録に失敗しました"
+      render "groups/new"
+    end
+  end
+
+  def show
+    @group = Group.find(params[:id])
+  end
+
+  def edit
+    @group = Group.find(params[:id])
+  end
+
+  def update
+    @group = Group.find(params[:id])
+    if @group.update(group_params)
+      flash[:notice] = "グループを更新しました"
+      redirect_to user_path(current_user.id)
+    else
+      flash.now[:alert] = "グループの更新に失敗しました"
+      render "groups/edit"
+    end
+  end
+
+  def destroy
+    @group = Group.find(params[:id])
+    @group.destroy
+    flash[:notice] = "グループを削除しました"
+    redirect_to user_path(current_user.id)
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:name, { user_ids: [] })
+  end
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
   def show
+    @groups = current_user.groups
   end
 end
