54f38e75e99f113e79e8faf5ce985e7300bc7887
ksk-M <bestwedd1@gmail.com>
2024-09-21 12:37:58 +0900

    [fix] ユーザー削除の際のエラーを修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  has_many :group_users
-  has_many :groups, through: :group_users
+  has_many :group_users, dependent: :destroy
+  has_many :groups, through: :group_users, dependent: :destroy
   has_one_attached :icon
 
   validates :name, presence: :true, length: { maximum: 15 }
