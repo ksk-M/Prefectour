ede5a9cfaa0fe1c28d63dc53c2cb99c1b222a961
ksk-M <bestwedd1@gmail.com>
2024-09-29 12:04:55 +0900

    [add] 詳細画面にも一覧を表示しユーザビリティ向上

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -22,6 +22,7 @@ class DestinationsController < ApplicationController
 
   def show
     @destination = Destination.find(params[:id])
+    @destinations = current_user.destinations
   end
 
   def edit
