989ca789c4eb0f558ac120d977169369553cbdce
ksk-M <bestwedd1@gmail.com>
2024-10-09 23:27:44 +0900

    [change] 無駄なAIプラン生成を避けるため、バリデーションをパスした場合のみ生成を行うよう変更

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -8,8 +8,6 @@ class PlansController < ApplicationController
 
   def create
     selected_destination_names = Destination.where(id: plan_params[:destination_ids].reject(&:blank?)).pluck(:name)
-    start_date = plan_params[:start_date]
-    end_date = plan_params[:end_date]
 
     if selected_destination_names.blank?
       flash[:alert] = "目的地が選択されていません。"
@@ -17,14 +15,14 @@ class PlansController < ApplicationController
     end
 
     @plan = Plan.new(plan_params)
-    @plan.proposed_plan = Plan.generate_travel_plan(selected_destination_names, start_date, end_date)
-
     if @plan.save
+      # バリデーションをパスした場合のみAI提案プランを生成
+      @plan.update(proposed_plan: Plan.generate_travel_plan(selected_destination_names, @plan.start_date, @plan.end_date))
       flash[:notice] = "「#{@plan.title}」を作成しました"
       redirect_to plan_path(@plan.id)
     else
-      @group_id = @plan.group_id
-      @selected_destinations = Destination.where(id: @plan.destination_ids)
+      @group_id = plan_params[:group_id]
+      @selected_destinations = Destination.where(id: plan_params[:destination_ids])
       flash.now[:alert] = "旅行計画の作成に失敗しました"
       render "plans/new"
     end
