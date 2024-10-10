79996b36b1a1560bf533ff175077f711e8f4b39d
ksk-M <bestwedd1@gmail.com>
2024-10-10 15:41:05 +0900

    [clean] ルーティングを必要なアクションに限定

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: 'home#top'
   devise_for :users
   resources :users, only: :show
-  resources :groups
+  resources :groups, only: [:new, :create, :show, :edit, :update, :destroy]
   resources :destinations
-  resources :plans
+  resources :plans, only: [:new, :create, :show, :edit, :update, :destroy]
 end
