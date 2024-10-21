60394275961e36cd6e907d5e3e10f03b71ae5f80
ksk-M <bestwedd1@gmail.com>
2024-10-21 15:50:15 +0900

    [change] 写真の枚数制限に対するカスタムバリデーションを作成 active_storage_validationsのテストマッチャがサポートされていないため

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -5,12 +5,19 @@ class Plan < ApplicationRecord
   has_many_attached :images
 
   validates :title, presence: true, length: { maximum: 25 }
-  validates :images, limit: { min: 0, max: 3, message: 'は3枚以下にしてください。' }, content_type: { in: ['image/png', 'image/jpeg'], message: 'の形式が対応していません。対応形式はPNGまたはJPEGです。' }
+  validates :images, content_type: { in: ['image/png', 'image/jpeg'], message: 'の形式が対応していません。対応形式はPNGまたはJPEGです。' }
   validate :start_date_is_before_end_date
+  validate :no_more_than_3_photos
 
   def start_date_is_before_end_date
     if start_date.present? && end_date.present? && start_date > end_date
       errors.add(:end_date, "は開始日より後の日付にしてください。")
+    end
+  end
+
+  def no_more_than_3_photos
+    if images.attached? && images.count > 3
+      errors.add(:images, "は3枚以内にしてください。")
     end
   end
 
