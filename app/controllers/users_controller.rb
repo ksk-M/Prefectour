6f97bb61e1a89189597cc269ba90776524567194
ksk-M <bestwedd1@gmail.com>
2024-10-23 16:41:47 +0900

    [add] 次の旅行までのカウントダウン機能を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,11 @@ class UsersController < ApplicationController
   def show
     @groups = current_user.groups
     @user_pref = current_user.prefecture_code
-    user_plans = current_user.groups.joins(:plans).pluck('plans.id')
+    user_plans = @groups.joins(:plans).pluck('plans.id')
     visited_addresses = Destination.joins(:plan_destinations).where(plan_destinations: { plan_id: user_plans },
                                                                     status: "訪問済").distinct.pluck(:address)
     @visited_pref_codes = visited_addresses.map { |address| Destination.extract_prefecture_code(address) }.uniq.compact
+    @latest_plan = Plan.joins(:group).where(groups: { id: current_user.group_ids }).order(start_date: :desc).first
+    @days_left = (@latest_plan.start_date - Date.current).to_i
   end
 end
