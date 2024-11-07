b5cb265a1d8d32c62f01d0959bb897424a8f7416
ksk-M <bestwedd1@gmail.com>
2024-11-07 23:33:11 +0900

    [add] ゲストログイン機能を追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   root to: 'home#top'
   get '/about', to: 'pages#about'
   devise_for :users
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+  end
   resources :users, only: :show
   resources :groups, only: [:new, :create, :show, :edit, :update, :destroy]
   resources :destinations
